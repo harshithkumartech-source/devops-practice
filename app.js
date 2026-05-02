231c60d31666d846d53327b198d77be51d272f63
harshithkumartech-source <harshithkumar.tech@gmail.com>
2026-05-02 16:10:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 // app.js - Simple calculator functions
 // These are the functions our tests will test
-
 function add(a, b) {
   return a + b;
 }
-
 function subtract(a, b) {
   return a - b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   if (b === 0) {
     throw new Error('Cannot divide by zero');
@@ -20,4 +16,13 @@ function divide(a, b) {
   return a / b;
 }
 
-module.exports = { add, subtract, multiply, divide };
+// Bad code to test ESLint rules
+var x = 5
+
+// Vulnerable function - intentionally insecure for DevSecOps learning
+function getUserById(userId) {
+  const query = "SELECT * FROM users WHERE id = " + userId
+  return query
+}
+
+module.exports = { add, subtract, multiply, divide, getUserById };
